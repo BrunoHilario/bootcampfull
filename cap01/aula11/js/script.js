fc273abd8448b910703dd3c1a5622290b346ea20
BrunoHilario <bruno.hilario@id.uff.br>
2020-10-02 18:10:57 -0300

    01102020

--- cap01/aula11/js/script.js
+++ cap01/aula11/js/script.js
@@ -6,13 +6,17 @@ window.addEventListener('load', () => {
   doFind();
   doSome();
   doEvery();
+  doSort();
+  doSpread();
+  doRest();
+  doDestructuring();
 });
 
 function doMap() {
   const nameEmailArray = people.results.map((person) => {
     return {
       name: person.name,
-      age: person.dob.age,
+      email: person.email,
     };
   });
   console.log(nameEmailArray);
@@ -22,9 +26,9 @@ function doMap() {
 
 function doFilter() {
   const olderThan18 = people.results.filter((person) => {
-    return (person.name.first = 'z');
+    return person.dob.age > 60;
   });
-  // console.log(olderThan18);
+  console.log(olderThan18);
 }
 
 function doForEach() {
@@ -37,33 +41,74 @@ function doForEach() {
       person.name.last.length;
   });
 
-  /// console.log(mappedPeople);
+  console.log(mappedPeople);
 }
 
 function doReduce() {
   const totalAges = people.results.reduce((accumulator, current) => {
     return accumulator + current.dob.age;
   }, 0);
-  //  console.log(totalAges);
+  console.log(totalAges);
 }
 
 function doFind() {
   const found = people.results.find((person) => {
-    return person.location.city === 'Sydney';
+    return person.location.state === 'Minas Gerais';
   });
-  //  console.log(found);
+  console.log(found);
 }
 
 function doSome() {
   const found = people.results.some((person) => {
-    return person.location.city === 'Sydney';
+    return person.location.state === 'Minas Gerais';
   });
 
-  //console.log(found);
+  console.log(found);
 }
 function doEvery() {
   const every = people.results.every((person) => {
-    return person.nat === 'US';
+    return person.nat === 'BR';
   });
-  //console.log(every);
+  console.log(every);
 }
+
+function doSort() {
+  const mappedNames = people.results
+    .map((person) => {
+      return {
+        name: person.name.first,
+      };
+    })
+    .filter((person) => person.name.startsWith('A'))
+    .sort((a, b) => {
+      return a.name.localeCompare(b.name);
+      //return b.name.length-a.name.length;
+    });
+
+  console.log(mappedNames);
+}
+
+function doSpread() {
+  const marriedMen = people.results.filter(
+    (person) => person.name.title === 'Mr'
+  );
+  const marriedWomen = people.results.filter(
+    (person) => person.name.title === 'Ms'
+  );
+
+  const marriedPeople = [...marriedMen, ...marriedWomen, { msg: 'oi' }];
+
+  console.log(marriedPeople);
+}
+
+function doRest() {
+  console.log(infiniteSum(1, 2));
+  console.log(infiniteSum(1, 2, 1000));
+  console.log(infiniteSum(1, 2, 1, 2, 3, 4, 5, 36, 7));
+}
+
+function infiniteSum(...numbers) {
+  return numbers.reduce((acc, curr) => acc + curr, 0);
+}
+
+function doDestructuring() {}
